2f5f5af2db0aad22a43f5a4d982a63b64d1a7d92
jaydeetee97 <pricesmayvary302@gmail.com>
2025-05-22 18:23:46 -0400

    troubleshooting

--- backend/server.ts
+++ backend/server.ts
@@ -25,11 +25,11 @@ import { createActivity } from "./helpers/createActivity";
 import { isGuessRight } from "./helpers/isGuessRight";
 
 const app = express();
-const port = 5500;
+const port = 3000;
 const server = app.listen(port);
 const io = new Server(server, {
   cors: {
-    origin: "*", // or '*' for all origins (not recommended in prod)
+    origin: "http://localhost:1024", // or '*' for all origins (not recommended in prod)
     methods: ["GET", "POST"],
   },
 });
--- frontend/modules/PlayerRoom.tsx
+++ frontend/modules/PlayerRoom.tsx
@@ -17,7 +17,7 @@ import TopThreePlayers from "./PlayerRoom/TopThreePlayers";
 
 export default function PlayerRoom({ params }: { params: { roomId: string } }) {
   const router = useRouter();
-  const { emit, on } = useSocket("http://0.0.0.0:5500");
+  const { emit, on } = useSocket("http://localhost:3000");
   const [playerName, setPlayerName] = React.useState("");
   const [guess, setGuess] = React.useState("");
   const roomId = params.roomId?.trim();
