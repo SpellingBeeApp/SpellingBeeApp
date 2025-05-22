cd9e33bed90d47b175e14808bfc203f67c064c14
jaydeetee97 <pricesmayvary302@gmail.com>
2025-05-22 17:33:50 -0400

    port number

--- frontend/modules/LandingPage.tsx
+++ frontend/modules/LandingPage.tsx
@@ -16,7 +16,7 @@ export default function LandingPage() {
   const [playerName, setPlayerName] = React.useState("");
   const [roomCode, setRoomCode] = React.useState("");
   const [activeTab, setActiveTab] = React.useState("join");
-  const { socket } = useSocket("http://secretbee.zipcode.rocks:1024/");
+  const { socket } = useSocket("http://secretbee.zipcode.rocks:5500");
 
   /**
    * creates a room for the spelling bee
